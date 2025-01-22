e41ec9eb8af0340b168f571b1f58df68c33a5f71
its-ani <guptaanirudh712@gmail.com>
2025-01-22 23:36:43 +0530

    Modifications in Controller

--- src/main/java/dev/anirudh/Controllers/TicketController.java
+++ src/main/java/dev/anirudh/Controllers/TicketController.java
@@ -2,10 +2,18 @@ package dev.anirudh.Controllers;
 
 import dev.anirudh.DTOs.GenerateTicketRequestDto;
 import dev.anirudh.DTOs.GenerateTicketResponseDto;
+import dev.anirudh.DTOs.ResponseStatus;
 import dev.anirudh.Models.ParkingSpotAssignmentStrategyType;
+import dev.anirudh.Models.Ticket;
 import dev.anirudh.Models.VehicleType;
+import dev.anirudh.Service.TicketService;
 
 public class TicketController {
+    private TicketService ticketService;
+    public TicketController(TicketService ticketService) {
+        this.ticketService = ticketService;
+    }
+
     public GenerateTicketResponseDto generateTicket(GenerateTicketRequestDto generateTicketRequestDto){
 
         String vehicleNumber = generateTicketRequestDto.getVehicleNumber();
@@ -14,7 +22,23 @@ public class TicketController {
         VehicleType vehicleType = generateTicketRequestDto.getVehicleType();
         ParkingSpotAssignmentStrategyType parkingSpotAssignmentStrategyType = generateTicketRequestDto.getParkingSpotassignmentStrategyType();
 
+        GenerateTicketResponseDto generateTicketResponseDto = new GenerateTicketResponseDto();
+        try{
+            Ticket ticket = ticketService.genereateTicket(
+                    vehicleNumber,
+                    ownerName,
+                    gateId,
+                    vehicleType,
+                    parkingSpotAssignmentStrategyType
+            );
 
-        return null;
+            generateTicketResponseDto.setTicket(ticket);
+            generateTicketResponseDto.setResponseStatus(ResponseStatus.SUCCESS);
+        }
+        catch (Exception e){
+            generateTicketResponseDto.setResponseStatus(ResponseStatus.FAILED);
+        }
+
+        return generateTicketResponseDto;
     }
 }
--- src/main/java/dev/anirudh/Main.java
+++ src/main/java/dev/anirudh/Main.java
@@ -1,8 +1,26 @@
 package dev.anirudh;
 
+import dev.anirudh.Controllers.TicketController;
+import dev.anirudh.DTOs.GenerateTicketRequestDto;
+import dev.anirudh.Models.ParkingSpotAssignmentStrategyType;
+import dev.anirudh.Models.VehicleType;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+
+        GenerateTicketRequestDto generateTicketRequestDto = new GenerateTicketRequestDto();
+        TicketController ticketController = new TicketController();
+
+        generateTicketRequestDto.setGateId(1L);
+        generateTicketRequestDto.setOwnerName("Anirudh");
+        generateTicketRequestDto.setParkingSpotassignmentStrategyType(ParkingSpotAssignmentStrategyType.RANDOM);
+        generateTicketRequestDto.setVehicleNumber("DL 8S BP 5650");
+        generateTicketRequestDto.setVehicleType(VehicleType.TWO_WHEELER);
+
+
+        System.out.println("Your Ticket is created " + ticketController.generateTicket(generateTicketRequestDto));
+
     }
 }

f3977c968f548d45124ed8571e5bc1385d4df661
its-ani <guptaanirudh712@gmail.com>
2025-01-23 00:42:00 +0530

    Manually created dependecies & Response converte to JSON

--- src/main/java/dev/anirudh/Main.java
+++ src/main/java/dev/anirudh/Main.java
@@ -1,13 +1,18 @@
 package dev.anirudh;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import dev.anirudh.Controllers.TicketController;
 import dev.anirudh.DTOs.GenerateTicketRequestDto;
-import dev.anirudh.Models.ParkingSpotAssignmentStrategyType;
-import dev.anirudh.Models.VehicleType;
+import dev.anirudh.DTOs.GenerateTicketResponseDto;
+import dev.anirudh.Models.*;
 import dev.anirudh.Repository.GateRepository;
 import dev.anirudh.Repository.TicketRepository;
 import dev.anirudh.Repository.VehicleRepository;
 import dev.anirudh.Service.TicketService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,20 +20,72 @@ public class Main {
         System.out.println("Hello world!");
 
         GenerateTicketRequestDto generateTicketRequestDto = new GenerateTicketRequestDto();
+        Gate gate = new Gate();
+        Vehicle vehicle = new Vehicle();
+        Operator operator = new Operator();
         GateRepository gateRepository = new GateRepository();
         TicketRepository ticketRepository = new TicketRepository();
         VehicleRepository vehicleRepository = new VehicleRepository();
         TicketService ticketService = new TicketService(gateRepository, vehicleRepository, ticketRepository);
         TicketController ticketController = new TicketController(ticketService);
 
+
+//      Operator
+//      private String name;
+        operator.setId(1L);
+        operator.setName("Vivek");
+        operator.setCreatedAt(new Date());
+        operator.setLastModifiedAt(operator.getCreatedAt());
+
+
+//        Gate
+//        private Operator operator;
+//        private String gateNumber;
+//        private GateType gateType;
+
+        gate.setId(1L);
+        gate.setGateNumber("1");
+        gate.setGateType(GateType.ENTRY);
+        gate.setOperator(operator);
+        gate.setCreatedAt(new Date());
+        gate.setLastModifiedAt(gate.getCreatedAt());
+        gateRepository.save(gate);
+
+//        Vehicle
+//        private String VehicleNumber;
+//        private String ownerName;
+//        private VehicleType vehicleType;
+        vehicle.setId(1L);
+        vehicle.setVehicleNumber("DL 8S BP 5650");
+        vehicle.setCreatedAt(new Date());
+        vehicle.setLastModifiedAt(vehicle.getCreatedAt());
+        vehicle.setOwnerName("Anirudh");
+        vehicle.setVehicleType(VehicleType.TWO_WHEELER);
+        vehicleRepository.save(vehicle);
+
+
+
+//        GenerateTicket
         generateTicketRequestDto.setGateId(1L);
         generateTicketRequestDto.setOwnerName("Anirudh");
         generateTicketRequestDto.setParkingSpotassignmentStrategyType(ParkingSpotAssignmentStrategyType.RANDOM);
         generateTicketRequestDto.setVehicleNumber("DL 8S BP 5650");
         generateTicketRequestDto.setVehicleType(VehicleType.TWO_WHEELER);
 
-//        ticketController.generateTicket(generateTicketRequestDto);
-        System.out.println("Your Ticket is created " + ticketController.generateTicket(generateTicketRequestDto));
 
+        GenerateTicketResponseDto generateTicketResponseDto = ticketController.generateTicket(generateTicketRequestDto);
+        try {
+            String json = "{\"ticket\":{\"id\":null,\"createdAt\":null,\"lastModifiedAt\":null,\"entryTime\":1737572467590,\"vehicle\":{\"id\":1,\"createdAt\":null,\"lastModifiedAt\":null,\"ownerName\":\"Anirudh\",\"vehicleType\":\"TWO_WHEELER\",\"vehicleNumber\":\"DL 8S BP 5650\"},\"parkingSpot\":{\"id\":null,\"createdAt\":null,\"lastModifiedAt\":null,\"spotNumber\":null,\"vehicleType\":null,\"parkingSpotStatus\":null,\"vehicle\":null},\"gate\":{\"id\":1,\"createdAt\":1737572467588,\"lastModifiedAt\":1737572467588,\"operator\":{\"id\":1,\"createdAt\":1737572467588,\"lastModifiedAt\":1737572467588,\"name\":\"Vivek\"},\"gateNumber\":\"1\",\"gateType\":\"ENTRY\"}},\"responseStatus\":\"SUCCESS\"}";
+            ObjectMapper objectMapper = new ObjectMapper();
+            Object jsonObject = objectMapper.readValue(json, Object.class);
+            ObjectWriter writer = objectMapper.writerWithDefaultPrettyPrinter();
+            String prettyJson = writer.writeValueAsString(jsonObject);
+            System.out.println(prettyJson);
+        }
+        catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Your Ticket is created " + generateTicketResponseDto);
     }
 }
--- src/main/java/dev/anirudh/Models/Operator.java
+++ src/main/java/dev/anirudh/Models/Operator.java
@@ -1,5 +1,13 @@
 package dev.anirudh.Models;
 
 public class Operator extends BaseModel {
+    private String name;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }

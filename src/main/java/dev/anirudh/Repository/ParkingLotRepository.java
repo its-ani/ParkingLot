b422b4574f6e49125829f556425aa423914b4d42
its-ani <guptaanirudh712@gmail.com>
2025-02-19 20:32:36 +0530

    Fixed ParkingLotRepository

--- src/main/java/dev/anirudh/Repository/ParkingLotRepository.java
+++ src/main/java/dev/anirudh/Repository/ParkingLotRepository.java
@@ -12,32 +12,27 @@ public class ParkingLotRepository {
     private final Map<Long, ParkingLot> parkingLotStorage = new HashMap<>();
     private final Map<Long, Long> gateToParkingLotMap = new HashMap<>();
 
-    @Override
     public int getCapacityForVehicleType(VehicleType type, ParkingFloor floor) {
         return (int) floor.getParkingSpots().stream()
                 .filter(spot -> spot.getVehicleType().equals(type))
                 .count();
     }
 
-    @Override
     public Optional<ParkingLot> getParkingLotByGateId(long gateId) {
         Long parkingLotId = gateToParkingLotMap.get(gateId);
         return parkingLotId != null ? Optional.ofNullable(parkingLotStorage.get(parkingLotId)) : Optional.empty();
     }
 
-    @Override
     public Optional<ParkingLot> getParkingLotById(long id) {
         return Optional.ofNullable(parkingLotStorage.get(id));
     }
 
-    @Override
     public ParkingLot save(ParkingLot parkingLot) {
         parkingLotStorage.put(parkingLot.getId(), parkingLot);
         parkingLot.getGates().forEach(gate -> gateToParkingLotMap.put(gate.getId(), parkingLot.getId()));
         return parkingLot;
     }
 
-    @Override
     public List<ParkingFloor> getParkingFloors(ParkingLot parkingLot) {
         return parkingLot.getParkingFloorList();
     }
